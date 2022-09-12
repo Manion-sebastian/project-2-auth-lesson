1e26399bda4eeb12cf7162108c126e3f5d0e70e6
Manion-sebastian <Sjmanion123@gmail.com>
2022-09-12 12:27:19 -0700

    added routes and login form

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../models')
 
 router.get('/new', (req,res) => {
     // render a form to create a new user
@@ -8,11 +9,33 @@ router.get('/new', (req,res) => {
 
 router.post('/', async (req,res) => {
     try {
-        console.log(req.body)
-        res.send('posts user')
+        //create new user
+        const newUser = await db.user.create(req.body)
+        //store that new users id as a cookie in browser
+        res.cookie('userId', newUser.id) 
+        res.redirect('/')
+
     } catch(err) {
         console.warn(err)
     }
+})
+
+// GET /users/login -- show a login form to the user
+
+router.get('/login', (req,res) => {
+    res.render('users/login')
+})
+
+// POST /users/login -- accept a payload of form data and use it to  log a user in
+
+router.post('/login', (req,res) => {
+    res.send('log the user in')
+})
+
+// GET /users/logout -- log out a user by clearing the cookie
+
+router.get('/logout', (req,res) => {
+    res.send('log the user out')
 })
 
 module.exports = router
